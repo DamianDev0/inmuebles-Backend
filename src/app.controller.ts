64e56223f78e436a27ebbaff4d11b1a60d2c42ca
Mahi_G13 <maicolgallego14@gmail.com>
2024-08-28 17:45:11 -0400

    Post in process

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreatePropertyDto } from './dtos/createProperty/CreateProperty.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+ @Post()
+ async postProperty(@Body() property:CreatePropertyDto){
+    return this.appService.createNewProperty(property)
+ }
+
 }
