b2b1adea9a35afa6506888e574b16f760a38cd6c
damian <dg997678@gmail.com>
2024-08-29 08:18:43 -0500

    add global pipes, swagger config, globalprefix

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,9 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { CreatePropertyDto } from './dtos/createPropertyDTO/createProperty.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('properties')
 @Controller('properties')
 export class AppController {
   constructor(private readonly appService: AppService) {}
