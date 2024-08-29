6a49f21522679ef4d9aab4684c78d976bf0c1b96
Scorhenao <samcorhenao@gmail.com>
2024-08-29 17:21:17 -0500

    dto for get all properties

--- src/dtos/GetProperty/getAllProperties.dto.ts
+++ src/dtos/GetProperty/getAllProperties.dto.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@nestjs/common";
-import {  IsBtcAddress, IsNotEmpty,  IsString, IsUUID } from "class-validator";
+import {  IsBtcAddress, IsDate, IsEmpty, IsNotEmpty,  IsNumber,  IsString, IsUUID } from "class-validator";
+import { PropertyMedia } from "src/entities/propertyMedia.entity";
 
 @Injectable()
 export class GetAllPropertiesDto{
@@ -21,5 +22,51 @@ export class GetAllPropertiesDto{
     @IsBtcAddress()
     adrress:string;
 
+    @IsString()
+    @IsNotEmpty()
+    city:string;
 
+    @IsString()
+    @IsNotEmpty()
+    deparment:string;
+
+    @IsNumber()
+    @IsNotEmpty()
+    price:number;
+
+    @IsNumber()
+    @IsNotEmpty()
+    size:number;
+
+    @IsNumber()
+    @IsNotEmpty()
+    bedrooms:number;
+
+    @IsNumber()
+    @IsNotEmpty()
+    bathrooms:number;
+
+    @IsNumber()
+    @IsEmpty()
+    garage:number;
+
+    @IsString()
+    @IsNotEmpty()
+    property_type_id:string
+
+    @IsString()
+    @IsNotEmpty()
+    media:string
+
+    @IsString()
+    @IsEmpty()
+    owner_id:string
+
+    @IsDate()
+    @IsNotEmpty()
+    createdAt:Date
+
+    @IsDate()
+    @IsNotEmpty()
+    updatedAt:Date
 }
