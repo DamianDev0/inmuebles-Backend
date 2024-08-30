0748ce282615f48dec5c826346f3053a5f88416f
Scorhenao <samcorhenao@gmail.com>
2024-08-29 19:25:32 -0500

    get all service READY

--- src/services/GetProperty/getAllProperties.service.ts
+++ src/services/GetProperty/getAllProperties.service.ts
@@ -1,16 +1,35 @@
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { IcreateProperty } from "src/common/interfaces/CreateProperty /createProperty.interface";
-import { CreatePropertyDto } from "src/dtos/createProperty/CreateProperty.dto";
-import { Property } from "src/entities/property.entity";
-import { Repository } from "typeorm/repository/Repository";
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { IgetAllProperties } from 'src/common/interfaces/GetProperty/getAllProperties.interface';
+import { GetAllPropertiesDto } from 'src/dtos/GetProperty/getAllProperties.dto';
+import { Property } from 'src/entities/property.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class GetAllPropertiesService implements IcreateProperty{
-    constructor(@InjectRepository(Property) private readonly propertyRepository:Repository<Property>){}
+export class GetAllPropertiesService implements IgetAllProperties {
+    constructor(
+        @InjectRepository(Property)
+        private readonly propertyRepository: Repository<Property>,
+    ) {}
 
-   async createProperty(property: CreatePropertyDto): Promise<Property> {
-    const newProperty = this.propertyRepository.create(property)
-    return await this.propertyRepository.save(newProperty);  // Save the property to the database.
-   }
+    async getAllProperties(dto: GetAllPropertiesDto): Promise<Property[]> {
+        // Crea un objeto de consulta basado en el DTO
+        const query = this.propertyRepository.createQueryBuilder('property');
+
+        if (dto.id) {
+            query.andWhere('property.id = :id', { id: dto.id });
+        }
+
+        if (dto.title) {
+            query.andWhere('property.title = :title', { title: dto.title });
+        }
+
+        if (dto.description) {
+            query.andWhere('property.description = :description', { description: dto.description });
+        }
+
+        // Agrega más filtros según los campos del DTO
+
+        return await query.getMany(); // Usa getMany() para obtener una lista de propiedades
+    }
 }
